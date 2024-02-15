5b2f3f636c73ca19ba5e4268cb97902cd2ff0948
Iryna Romashko <137172921+it-tinyangel@users.noreply.github.com>
2024-02-15 21:04:22 +0200

    First commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
 	const getS = selector => document.querySelector(selector);
+	// const getAllS = selector => document.querySelectorAll(selector);
+	const applyStyleForm = document.forms['applyStyleForm'];
+	
 	const colors = ['black', 'gray', 'white', 'cyan', 'magenta', 'yellow', 'red', 'green', 'blue'];
 
 	getS('.btn-edit').addEventListener('click', () => {
 		getS('.edit-block').classList.add('show');
 		getS('.style-block').classList.remove('show');
-		getS('textarea.edit-area').value = getS('.top-block').innerHTML;
+		getS('.edit-area').value = getS('.top-block').innerHTML;
 	});
 
 	getS('.btn-style').addEventListener('click', () => {
 		getS('.style-block').classList.add('show');
 		getS('.edit-block').classList.remove('show');
+	})
+
+	getS('.btn-save').addEventListener('click', () => {
+		getS('.edit-block').classList.remove('show');
+		getS('.top-block').innerHTML = getS('textarea.edit-area').value;
+	})
+
+	// getAllS('input[name="fontSize"]').forEach(function (radio) {
+	// 	radio.addEventListener('click', function () {
+	// 		getS('.top-block').style.fontSize = event.target.value;
+	// 	});
+	// });
+
+	applyStyleForm.elements['fontSize'].forEach(function (radio) {
+		radio.addEventListener('click', function () {
+			getS('.top-block').style.fontSize = event.target.value;
+		});
+	});
+
+	applyStyleForm.elements['fontFamily'].addEventListener('click', () => {
+		getS('.top-block').style.fontFamily = event.target.value;
 	})
 
 	getS('.btn-text-color').addEventListener('click', () => {
